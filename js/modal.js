4956ce2500e0e2a5ef10723fb7d50b8fba5adb16
zee1013 <dmswl1251@gmail.com>
2026-04-01 10:22:53 +0900

    modal창 html, css, js

--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,25 @@
-window.addEventListener("load",function(){
-    //go top 스크롤 이벤트
-    //변수 선언
-    const goTop = this.document.querySelector("#gotop")
-    this.window.addEventListener("scroll", ()=>{
-        if(this.document.documentElement.scrollTop > 1000){
-            goTop.style.display = "block"
-        }
-        else{
-            goTop.style.display = "none"
-        }
-    })
-    //go top 버튼 클릭시 위로 스크롤
-    goTop.addEventListener("click",()=>{
-        this.window.scrollTo({
-            top:0,
-            // behavior: "smooth",
-        })
-    })
-})
+window.addEventListener("load", function () {
+  // modal창 닫기
+  const modalWrap = this.document.querySelector(".modal-wrap");
+  const modalClose = this.document.querySelector(".modalClose");
+  modalClose.addEventListener("click", () => {
+    modalWrap.style.display = "none";
+  });
+  //go top 스크롤 이벤트
+  //변수 선언
+  const goTop = this.document.querySelector("#gotop");
+  this.window.addEventListener("scroll", () => {
+    if (this.document.documentElement.scrollTop > 1000) {
+      goTop.style.display = "block";
+    } else {
+      goTop.style.display = "none";
+    }
+  });
+  //go top 버튼 클릭시 위로 스크롤
+  goTop.addEventListener("click", () => {
+    this.window.scrollTo({
+      top: 0,
+      // behavior: "smooth",
+    });
+  });
+});
